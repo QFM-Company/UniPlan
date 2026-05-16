e6bc41b0160f02c7ec5a5f0ffd9796e73fb12d02
Fares <fares.oyion123@gmail.com>
2026-05-16 21:41:16 +0300

    split Sp Administrator logic to Funcs

--- docs/db-design/scripts/Adminstrater_SPs_Logic.sql
+++ docs/db-design/scripts/Adminstrater_SPs_Logic.sql
@@ -1,45 +1,91 @@
 USE [UniPlan];
 GO
 
-CREATE PROCEDURE SP_Admin_Profile_Insert 
 
- @IsActive bit , @PersonID int , @AdministratorID int out,
 
- @AccountName nvarchar(255) , @Password nvarchar(255) , @Email nvarchar(255) , @AccountID int out
-
- As
- Begin
-     SET NOCOUNT ON;
-
-IF @PersonID IS NULL OR @PersonID <= 0 
-BEGIN
-    RAISERROR('Invalid Person provided', 16, 1);
-    RETURN;
-END
-
-IF @IsActive IS NULL 
+Create Function ValidatAccountInfo (@AccountName nvarchar(50) , @Password nvarchar(255) , @Email nvarchar(255))
+returns bit
 Begin
-    set @IsActive = 1;
-end
 
 IF @AccountName IS NULL OR @Email IS NULL OR @Password IS NULL
 BEGIN
-    RAISERROR('AccountName, Email, and Password cannot be NULL', 16, 1);
-    RETURN;
-END
-
-IF NOT EXISTS (SELECT 1 FROM People WHERE PersonID = @PersonID)
-BEGIN
-    RAISERROR('PersonID %d does not exist', 16, 1, @PersonID);
-    RETURN;
+    RETURN 0;
 END
 
 -- Check for duplicate email/account name
 IF EXISTS (SELECT 1 FROM Accounts WHERE Email = @Email)
 BEGIN
-    RAISERROR('Email already exists', 16, 1);
-    RETURN;
+    RETURN 0;
 END
+
+return 1;
+End;
+go
+
+
+
+
+
+
+Create Function ValidatAdministratorInfo (@PersonID int)
+returns bit
+Begin
+
+
+IF @PersonID IS NULL OR @PersonID <= 0 
+BEGIN
+    RETURN 0;
+END
+
+
+
+IF NOT EXISTS (SELECT 1 FROM People WHERE PersonID = @PersonID)
+BEGIN
+    RETURN 0;
+END
+
+
+return 1;
+End;
+go
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE PROCEDURE SP_Admin_Profile_Insert 
+
+ @IsActive bit , @PersonID int , @AdministratorID int out,
+
+ @AccountName nvarchar(50) , @Password nvarchar(255) , @Email nvarchar(255) , @AccountID int out
+
+ As
+ Begin
+     SET NOCOUNT ON;
+
+	 IF @IsActive IS NULL 
+Begin
+    set @IsActive = 1;
+end
+
+
+        IF dbo.ValidatAccountInfo(@AccountName, @Password, @Email) = 0
+        BEGIN
+          RAISERROR('Account validation failed', 16, 1);
+	    END
+
+        -- Validate Person information
+        IF dbo.ValidatAdministrator(@PersonID) = 0
+        BEGIN
+             RAISERROR('Administrator validation failed', 16, 1);
+        END
 
 	 Begin Try
 	    Begin Transaction;
@@ -65,7 +111,4 @@ END
     END CATCH
 End;
 Go
-
-
-
 
