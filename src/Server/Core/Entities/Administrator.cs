9779996379c355aa923494cf2b2d5d28832a33cc
Fares <fares.oyion123@gmail.com>
2026-05-26 15:10:30 +0300

    Add Admin Files

---
+++ src/Server/Core/Entities/Administrator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    internal class Administrator
+    {
+        public int AdminID { get; set; }
+
+        public Person? _Person { get; set; }
+
+        public Account? _Account { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Administrator()
+        {
+        }
+
+        public Administrator(int adminID, Person? person, Account? account, bool isActive)
+        {
+            AdminID = adminID;
+            _Person = person;
+            _Account = account;
+            IsActive = isActive;
+        }
+
+    }
+}
---
+++ src/Server/Core/Interfaces/Repositories/IAdminRepository.cs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces.Repositories
+{
+    internal interface IAdminRepository
+    {
+        bool DeleteAdmin(int AdminID);
+        bool AddAdmin(Administrator Admin);
+        bool UpdateAdmin(Administrator Admin);
+        IReadOnlyCollection<Administrator> GetAllAdmins();
+        Administrator GetAdminByID(int AdminID);
+
+    }
+}
