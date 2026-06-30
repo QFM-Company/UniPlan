71c98d7c26fee883d66f599b7df2561d8b5531d0
Fares <fares.oyion123@gmail.com>
2026-06-30 11:44:06 +0300

    Add The Structure of the Validation Library And The External Hash Servese

---
+++ src/Server/Core/Interfaces/ExternalServices/IPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.ExternalServices
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+    }
+}
