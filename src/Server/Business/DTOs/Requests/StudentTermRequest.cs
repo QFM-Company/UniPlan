a04ee7c77a9132561c60ae4702440590d72950f2
Fares <fares.oyion123@gmail.com>
2026-07-02 07:49:47 +0300

    Add Validation To StudentTerm DTO Request

--- src/Server/Business/DTOs/Requests/StudentTermRequest.cs
+++ src/Server/Business/DTOs/Requests/StudentTermRequest.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Core.Entities;
+using Infrastructure.ExternalServices.Validation.Attributes;
 
 namespace Business.DTOs.Requests
 {
     public class StudentTermRequest
     {
+        [Required<int>("معرف الطالب مطلوب")]
+        [Range<int>("يجب ان يكون المعرف اكبر تماما من 0", 1, int.MaxValue)]
         public int StudentID { get; set; }
-
+        [Required<int>("معرف الفصل الدراسي مطلوب")]
+        [Range<int>("يجب ان يكون المعرف اكبر تماما من 0", 1, int.MaxValue)]
         public int AcademicTermID { get; set; }
 
         public StudentTermRequest(int studentID, int academicTermID)
