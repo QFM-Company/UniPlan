29bc1e74cce46b2d30043c1a5399202303ecf9b5
QUSAYALMADI <qusai99h@gmail.com>
2026-07-05 00:06:29 +0300

    add table  initially to user Control

---
+++ src/Client/Desktop/Controls/UserControls/DynamicDataGrid.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Controls.UserControls
+{
+    partial class DynamicDataGrid
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            dataGridView1 = new DataGridView();
+            DV_halls = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DV_halls).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(52, 84, 99);
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle4.ForeColor = Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(52, 84, 99);
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.GridColor = Color.FromArgb(52, 84, 99);
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = Color.FromArgb(52, 84, 99);
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle5.ForeColor = Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridViewCellStyle6.BackColor = Color.FromArgb(52, 84, 99);
+            dataGridViewCellStyle6.ForeColor = Color.White;
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridView1.Size = new Size(800, 500);
+            dataGridView1.TabIndex = 0;
+            // 
+            // DV_halls
+            // 
+            DV_halls.AllowUserToAddRows = false;
+            DV_halls.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DV_halls.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DV_halls.BackgroundColor = Color.FromArgb(244, 247, 249);
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = Color.FromArgb(52, 84, 99);
+            dataGridViewCellStyle7.Font = new Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle7.ForeColor = Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            DV_halls.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            DV_halls.ColumnHeadersHeight = 35;
+            DV_halls.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            DV_halls.EnableHeadersVisualStyles = false;
+            DV_halls.Location = new Point(-1, -2);
+            DV_halls.Name = "DV_halls";
+            DV_halls.RowHeadersWidth = 62;
+            DV_halls.Size = new Size(801, 502);
+            DV_halls.TabIndex = 3;
+            // 
+            // DynamicDataGrid
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(DV_halls);
+            Controls.Add(dataGridView1);
+            Name = "DynamicDataGrid";
+            Size = new Size(800, 500);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DV_halls).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridView DV_halls;
+    }
+}
--- src/Client/Desktop/Presentation/Forms/HallsManagementForm.Designer.cs
+++ src/Client/Desktop/Presentation/Forms/HallsManagementForm.Designer.cs
@@ -28,18 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             panel1 = new Panel();
+            searchControl1 = new Controls.UserControls.SearchControl();
             txtFloorNumber = new TextBox();
             txtBuildingName = new TextBox();
-            DV_halls = new DataGridView();
-            col1 = new DataGridViewTextBoxColumn();
-            col2 = new DataGridViewTextBoxColumn();
-            col3 = new DataGridViewTextBoxColumn();
-            col4 = new DataGridViewTextBoxColumn();
-            col5 = new DataGridViewButtonColumn();
-            searchControl1 = new Controls.UserControls.SearchControl();
             header1 = new Controls.UserControls.Header();
+            DV_halls = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)DV_halls).BeginInit();
             SuspendLayout();
@@ -54,6 +54,15 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(1170, 92);
             panel1.TabIndex = 1;
+            // 
+            // searchControl1
+            // 
+            searchControl1.BackColor = Color.White;
+            searchControl1.Location = new Point(94, 26);
+            searchControl1.Margin = new Padding(0);
+            searchControl1.Name = "searchControl1";
+            searchControl1.Size = new Size(250, 36);
+            searchControl1.TabIndex = 3;
             // 
             // txtFloorNumber
             // 
@@ -73,69 +82,6 @@
             txtBuildingName.Size = new Size(240, 35);
             txtBuildingName.TabIndex = 1;
             // 
-            // DV_halls
-            // 
-            DV_halls.AllowUserToAddRows = false;
-            DV_halls.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            DV_halls.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            DV_halls.BackgroundColor = Color.FromArgb(244, 247, 249);
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(52, 84, 99);
-            dataGridViewCellStyle2.Font = new Font("Segoe UI", 10.5F);
-            dataGridViewCellStyle2.ForeColor = Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
-            DV_halls.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            DV_halls.ColumnHeadersHeight = 35;
-            DV_halls.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            DV_halls.Columns.AddRange(new DataGridViewColumn[] { col1, col2, col3, col4, col5 });
-            DV_halls.EnableHeadersVisualStyles = false;
-            DV_halls.Location = new Point(105, 234);
-            DV_halls.Name = "DV_halls";
-            DV_halls.RowHeadersWidth = 62;
-            DV_halls.Size = new Size(1170, 542);
-            DV_halls.TabIndex = 2;
-            // 
-            // col1
-            // 
-            col1.HeaderText = "Hall ID";
-            col1.MinimumWidth = 8;
-            col1.Name = "col1";
-            // 
-            // col2
-            // 
-            col2.HeaderText = "Hall Name";
-            col2.MinimumWidth = 8;
-            col2.Name = "col2";
-            // 
-            // col3
-            // 
-            col3.HeaderText = "Building";
-            col3.MinimumWidth = 8;
-            col3.Name = "col3";
-            // 
-            // col4
-            // 
-            col4.HeaderText = "Floor";
-            col4.MinimumWidth = 8;
-            col4.Name = "col4";
-            // 
-            // col5
-            // 
-            col5.HeaderText = "Action";
-            col5.MinimumWidth = 8;
-            col5.Name = "col5";
-            // 
-            // searchControl1
-            // 
-            searchControl1.BackColor = Color.White;
-            searchControl1.Location = new Point(94, 26);
-            searchControl1.Margin = new Padding(0);
-            searchControl1.Name = "searchControl1";
-            searchControl1.Size = new Size(250, 36);
-            searchControl1.TabIndex = 3;
-            // 
             // header1
             // 
             header1.BackColor = Color.White;
@@ -145,14 +91,68 @@
             header1.TabIndex = 3;
             header1.Title = "UniPlan";
             // 
+            // DV_halls
+            // 
+            DV_halls.AllowUserToAddRows = false;
+            DV_halls.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DV_halls.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DV_halls.BackgroundColor = Color.FromArgb(244, 247, 249);
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(52, 84, 99);
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle1.ForeColor = Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            DV_halls.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            DV_halls.ColumnHeadersHeight = 35;
+            DV_halls.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            DV_halls.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            DV_halls.EnableHeadersVisualStyles = false;
+            DV_halls.Location = new Point(105, 248);
+            DV_halls.Name = "DV_halls";
+            DV_halls.RowHeadersWidth = 62;
+            DV_halls.Size = new Size(1170, 565);
+            DV_halls.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Hall ID";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Hall Name";
+            Column2.MinimumWidth = 8;
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Building";
+            Column3.MinimumWidth = 8;
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Floor";
+            Column4.MinimumWidth = 8;
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Action";
+            Column5.MinimumWidth = 8;
+            Column5.Name = "Column5";
+            // 
             // HallsManagement
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonFace;
             ClientSize = new Size(1378, 844);
-            Controls.Add(header1);
             Controls.Add(DV_halls);
+            Controls.Add(header1);
             Controls.Add(panel1);
             Name = "HallsManagement";
             StartPosition = FormStartPosition.CenterScreen;
@@ -167,16 +167,16 @@
         private Panel panel1;
         private TextBox txtFloorNumber;
         private TextBox txtBuildingName;
-        private DataGridView DV_halls;
-        private DataGridViewTextBoxColumn col1;
-        private DataGridViewTextBoxColumn col2;
-        private DataGridViewTextBoxColumn col3;
-        private DataGridViewTextBoxColumn col4;
-        private DataGridViewButtonColumn col5;
         private UniPlan.Controls.CustomButtons.UniPlanButton HallsBtn;
         private Controls.UserControls.Header header1;
         private UniPlan.Controls.CustomButtons.UniPlanButton uniPlanButton1;
         private UniPlan.Controls.CustomButtons.UniPlanButton uniPlanButton2;
         private Controls.UserControls.SearchControl searchControl1;
+        private DataGridView DV_halls;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
     }
 }
