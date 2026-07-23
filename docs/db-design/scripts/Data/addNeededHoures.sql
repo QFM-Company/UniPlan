06a22164d9d49e4cb96fbbd6da929b4cfc9b077b
Fares <fares.oyion123@gmail.com>
2026-07-23 13:08:36 +0300

    Add Hourse Logic to Proceduars And Fix Parent Major Problem

---
+++ docs/db-design/scripts/Data/addNeededHoures.sql
@@ -0,0 +1,18 @@
+﻿
+
+
+Alter Table Students
+Add [CompletedHours] int not null default 0;
+
+
+Alter Table Courses
+Add	[NeededHours] int not null default 0;
+
+update Courses
+set NeededHours = 100
+where CourseName = 'مشروع فصلي';
+
+
+update Courses
+set NeededHours = 125
+where CourseName = 'مشروع تخرج';
